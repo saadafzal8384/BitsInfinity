17d142825fe584048074825110b64ed37c125fad
Saad <mu.saadafzal@gmail.com>
2022-11-16 21:51:11 +0100

    Added  New Culture section

--- JS/script.js
+++ JS/script.js
@@ -12,16 +12,86 @@ menuToogleButton.addEventListener("click", () => {
   mobileNavigationContainer.style.display = "block";
   mobileNavigation.style.display = "flex";
   menuIcon.style.color = "white";
+  menuIcon.style.position = "fixed";
 });
 
 closeMenuBtn.addEventListener("click", () => {
-  // let width =
-  //   window.innerWidth /
-  //   parseFloat(getComputedStyle(document.querySelector("body"))["font-size"]);
   menuToogleButton.style.display = "block";
   closeMenuBtn.style.display = "none";
   mobileNavigationContainer.style.display = "none";
   mobileNavigation.style.display = "none";
-  menuIcon.style.color = "black";
+  if (document.body.classList.contains("dark-theme")) {
+    menuIcon.style.color = "white";
+    menuIcon.style.position = "fixed";
+  } else {
+    menuIcon.style.color = "black";
+    menuIcon.style.position = "fixed";
+  }
 });
 
+let mobileIcon = document.getElementById("toggle-mode-mobile");
+let mobileNavItem = document.querySelectorAll(".mobile-nav-item");
+
+mobileNavItem.forEach(function (mobileNavItems) {
+  mobileNavItems.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    menuToogleButton.style.display = "block";
+    closeMenuBtn.style.display = "none";
+    mobileNavigationContainer.style.display = "none";
+    mobileNavigation.style.display = "none";
+    if (document.body.classList.contains("dark-theme")) {
+      menuIcon.style.color = "white";
+      menuIcon.style.position = "fixed";
+    } else {
+      menuIcon.style.color = "black";
+      menuIcon.style.position = "fixed";
+    }
+    if (document.body.classList.contains("dark-theme")) {
+      toggleText.innerHTML = "Light Mode";
+      mobileIcon.classList.remove("fa-moon");
+      mobileIcon.classList.add("fa-sun");
+      homeImage.src = "IMG/home-white-bg.png";
+      $(".card").css("boxShadow", "none");
+    } else {
+      toggleText.innerHTML = "Dark Mode";
+      mobileIcon.classList.remove("fa-sun");
+      mobileIcon.classList.add("fa-moon");
+      homeImage.src = "IMG/new-bg.png";
+      // $(".card").css(
+      //   "boxShadow",
+      //   "0.6em 0.6em 1.2em #d2dcef, -0.5em -0.5em 1em #ffffff"
+      // );
+    }
+  });
+});
+
+mobileIcon.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  menuToogleButton.style.display = "block";
+  closeMenuBtn.style.display = "none";
+  mobileNavigationContainer.style.display = "none";
+  mobileNavigation.style.display = "none";
+  if (document.body.classList.contains("dark-theme")) {
+    menuIcon.style.color = "white";
+    menuIcon.style.position = "fixed";
+  } else {
+    menuIcon.style.color = "black";
+    menuIcon.style.position = "fixed";
+  }
+  if (document.body.classList.contains("dark-theme")) {
+    toggleText.innerHTML = "Light Mode";
+    mobileIcon.classList.remove("fa-moon");
+    mobileIcon.classList.add("fa-sun");
+    homeImage.src = "IMG/home-white-bg.png";
+    $(".card").css("boxShadow", "none");
+  } else {
+    toggleText.innerHTML = "Dark Mode";
+    mobileIcon.classList.remove("fa-sun");
+    mobileIcon.classList.add("fa-moon");
+    homeImage.src = "IMG/new-bg.png";
+    // $(".card").css(
+    //   "boxShadow",
+    //   "0.6em 0.6em 1.2em #d2dcef, -0.5em -0.5em 1em #ffffff"
+    // );
+  }
+});
